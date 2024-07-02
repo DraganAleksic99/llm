8556c226f42acf048bb3ad94f9a8e3e707297f66
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-07-03 01:44:49 +0200

    Update: add feed-forward network

--- architecture.py
+++ architecture.py
@@ -108,3 +108,35 @@ var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 
 print("Mean:\n", mean)
 print("Variance:\n", var)
+
+# GELU (Gaussian Error Linear Unit) activation function
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+# Feed-forward network  
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+ffn = FeedForward(GPT_CONFIG_124M)
+
+# input shape: [batch_size, num_token, emb_size]
+x = torch.rand(2, 3, 768) 
+out = ffn(x)
+print(out.shape)
