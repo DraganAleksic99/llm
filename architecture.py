4d5099d20fe87cf658ca65ad78b26b7f72875e18
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-07-03 00:28:09 +0200

    Update: add layer normalization

--- architecture.py
+++ architecture.py
@@ -12,6 +12,8 @@ GPT_CONFIG_124M = {
     "qkv_bias": False       # Query-Key-Value bias
 }
 
+# Simplified model architecture
+
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
@@ -59,3 +61,50 @@ class DummyLayerNorm(nn.Module):
     def forward(self, x):
         # This layer does nothing and just returns its input.
         return x
+    
+# Layer normalization, also known as LayerNorm, centers the activations of a neural network layer
+# around a mean of 0 and normalizes their variance to 1
+
+torch.manual_seed(123)
+
+# create 2 training examples with 5 dimensions (features) each
+batch_example = torch.randn(2, 5) 
+
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# Same as above as a class
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
