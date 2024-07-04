ed55458b9038ea435790bf98f520c74d8fec8aee
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-07-04 21:40:22 +0200

    Update: add transformer block

--- architecture.py
+++ architecture.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from multihead_attention import MultiHeadAttention
+from tokenizer import SimpleTokenizer
 
 # Configuration details for the 124 million parameter GPT-2 model
 GPT_CONFIG_124M = {
@@ -140,3 +142,48 @@ ffn = FeedForward(GPT_CONFIG_124M)
 x = torch.rand(2, 3, 768) 
 out = ffn(x)
 print(out.shape)
+
+# A transformer block combines the causal multi-head attention module with the linear layers 
+# and the feed forward neural network
+# In addition, the transformer block also uses dropout and shortcut connections
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"], 
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        return x
+    
+torch.manual_seed(123)
+
+x = torch.rand(2, 4, 768)  # Shape: [batch_size, num_tokens, emb_dim]
+block = TransformerBlock(GPT_CONFIG_124M)
+output = block(x)
+
+print("Input shape:", x.shape)
+print("Output shape:", output.shape)
